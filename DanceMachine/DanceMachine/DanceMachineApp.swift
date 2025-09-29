ec1c81c89540e5d02e84720cfeeea808adc94592
Velko <wlsgurrla716@naver.com>
2025-09-29 19:54:22 +0900

    🔧[Chore] - 프로젝트 추가

---
+++ DanceMachine/DanceMachine/DanceMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanceMachineApp.swift
+//  DanceMachine
+//
+//  Created by 김진혁 on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DanceMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
