d10a2c3034497114cdb01ec7d1652170c5b5a262
Velko <wlsgurrla716@naver.com>
2025-10-03 22:40:47 +0900

    🔧[Chore] - 파이어스토어 타입 분기처리 구현

---
+++ DanceMachine/DanceMachine/Common/Enum/Firestore/User/LoginType.swift
@@ -0,0 +1,13 @@
+//
+//  LoginType.swift
+//  DanceMachine
+//
+//  Created by 김진혁 on 10/3/25.
+//
+
+import Foundation
+
+// 로그인 타입
+enum LoginType: String, Codable {
+    case apple = "apple"
+}
---
+++ DanceMachine/DanceMachine/Common/Enum/Firestore/WriteStrategy.swift
@@ -0,0 +1,21 @@
+//
+//  WriteStrategy.swift
+//  DanceMachine
+//
+//  Created by 김진혁 on 10/3/25.
+//
+
+import Foundation
+
+/// 쓰기 전략 → 타임스탬프 필드명 매핑
+/// - create: created_at
+/// - update: updated_at
+/// - join: joined_at
+/// - userStrategy: last_login_at
+enum WriteStrategy: String {
+    case create = "created_at"
+    case update = "updated_at"
+    case join = "joined_at"
+    case userStrategy = "last_login_at"
+    case userUpdateStrategy
+}
