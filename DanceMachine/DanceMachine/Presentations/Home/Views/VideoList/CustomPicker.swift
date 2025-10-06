eadb8b01c1878a2802b1ba19c27653d1db9bddd8
jHoon99 <eldkf1597@naver.com>
2025-10-06 19:33:18 +0900

    [Feat]#10 - PhotosPicker 커스텀 구현

---
+++ DanceMachine/DanceMachine/Presentations/Home/Views/VideoList/CustomPicker.swift
@@ -0,0 +1,60 @@
+//
+//  CustomPicker.swift
+//  DanceMachine
+//
+//  Created by 조재훈 on 10/6/25.
+//
+
+import SwiftUI
+import Photos
+
+struct CustomPicker: View {
+  
+  @Binding var videos: [PHAsset]
+  @Binding var selectedAsset: PHAsset?
+  
+  let spacing: CGFloat
+  let itemWidth: CGFloat
+  
+  var body: some View {
+    LazyVGrid(
+      columns: [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+      ],
+      spacing: spacing
+    ) {
+#if DEBUG
+      if ProcessInfo.processInfo.environment ["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
+        ForEach(0..<24, id: \.self) { _ in
+          VideoThumbnailCell_Preview()
+            .frame(width: itemWidth, height: itemWidth)
+        }
+      } else {
+        ForEach(videos, id: \.localIdentifier) { asset in
+          VideoThumbnailCell(asset: asset, size: itemWidth)
+            .onTapGesture {
+              selectedAsset = asset
+            }
+        }
+      }
+#else
+      ForEach(videos, id: \.localIdentifier) { asset in
+        VideoThumbnailCell(asset: asset, size: itemWidth)
+          .onTapGesture {
+            print("\(itemWidth)")
+            selectedAsset = asset
+          }
+      }
+#endif
+    }
+    .padding(.top, 8)
+    
+  }
+}
+
+#Preview {
+  CustomPicker(videos: .constant([]), selectedAsset: .constant(PHAsset()), spacing: 1, itemWidth: 100)
+}
