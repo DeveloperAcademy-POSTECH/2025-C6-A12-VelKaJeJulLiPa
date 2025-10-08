61a111afa79cefcc81e20eac01d587099e61bb6a
jHoon99 <eldkf1597@naver.com>
2025-10-08 18:51:50 +0900

    ♻️[Refactor]#10 - 뷰 모델 분리
    VideoPickerVM -> 업로드 시 캐싱 하는게 아닌 재생 시 캐싱하는 방식으로

---
+++ DanceMachine/DanceMachine/Presentations/Home/ViewModels/Upload/VideoPickerVM.swift
@@ -0,0 +1,132 @@
+//
+//  VideoPickerVM.swift
+//  DanceMachine
+//
+//  Created by 조재훈 on 10/7/25.
+//
+
+import Foundation
+import Photos
+import AVKit
+
+@Observable
+final class VideoPickerVM {
+  
+  var selectedAsset: PHAsset? = nil
+  
+  var player: AVPlayer?
+  var videoTitle: String = ""
+  var videoThumbnail: UIImage? = nil
+  var videoDuration: Double = 0
+  var localVideoURL: URL? = nil
+  
+  var isLoading: Bool = false
+  
+  // MARK: 동영상 미리보기 로드
+  func loadVideo() {
+    self.isLoading = true
+    
+    let o = PHVideoRequestOptions()
+    o.isNetworkAccessAllowed = true
+    
+    PHImageManager.default().requestPlayerItem(
+      forVideo: selectedAsset ?? PHAsset(),
+      options: o) { item, _ in
+        if let pItem = item {
+          self.player = AVPlayer(playerItem: pItem)
+          self.isLoading = false
+        }
+      }
+  }
+}
+
+// MARK: 비디오 업로드 관련
+extension VideoPickerVM {
+  func exportVideo() {
+    self.isLoading = true
+    defer { self.isLoading = false }
+    
+    // PHAsset 에서 비디오 파일 추출
+    let o = PHVideoRequestOptions()
+    o.isNetworkAccessAllowed = true // iCloud에 있어도 다운로드
+    o.deliveryMode = .highQualityFormat // 화질
+    
+    PHImageManager.default().requestAVAsset(
+      forVideo: selectedAsset ?? PHAsset(),
+      options: o) {
+        av,
+        _,
+        _ in
+        guard let urlAsset = av as? AVURLAsset else { return }
+        
+        Task {
+          async let t = self.generateThumbnail(
+            from: urlAsset
+          )
+          async let d = self.getDuration(
+            from: urlAsset
+          )
+          
+          let (thumbnail, duration) = try await (t, d)
+        }
+        
+        //        Task {
+        //          // 임시 폴더로 복사
+        //          let fileName = "video_\(UUID().uuidString).mov"
+        ////          guard let asset = self.selectedAsset else { return }
+        ////          let sanitizedID = asset.localIdentifier
+        ////            .replacingOccurrences(of: "/", with: "_")
+        ////            .replacingOccurrences(of: "\\", with: "_")
+        ////          let fileName = "video_\(sanitizedID).mov"
+        //          let tempURL = URL.temporaryDirectory.appending(component: fileName)
+        //
+        //          do {
+        //            if FileManager.default.fileExists(atPath: tempURL.path()) {
+        //              try FileManager.default.removeItem(at: tempURL)
+        //              print("임시 디렉토리 삭제")
+        //            }
+        //            try FileManager.default.copyItem(at: urlAsset.url, to: tempURL)
+        //            print("\(tempURL.path()) 임시 디렉토리 추가")
+        //
+        //            async let t = self.generateThumbnail(
+        //              from: urlAsset
+        //            )
+        //            async let d = self.getDuration(
+        //              from: urlAsset
+        //            )
+        //
+        //            let (thumbnail, duration) = try await (t, d)
+        //
+        //            await MainActor.run {
+        //              self.localVideoURL = tempURL
+        //              self.videoThumbnail = thumbnail
+        //              self.videoDuration = duration
+        //            }
+        //          } catch {
+        //            print("비디오 복사 실패")
+        //          }
+        //        }
+      }
+  }
+  
+  private func generateThumbnail(
+    from asset: AVURLAsset
+  ) async throws-> UIImage? {
+    
+    let imageG = AVAssetImageGenerator(asset: asset)
+    imageG.appliesPreferredTrackTransform = true
+    
+    let t = CMTime(seconds: 1, preferredTimescale: 60)
+    
+    let (cgImage, _) = try await imageG.image(at: t)
+    return UIImage(cgImage: cgImage)
+  }
+  
+  private func getDuration(
+    from asset: AVURLAsset
+  ) async throws -> Double {
+    
+    let d = try await asset.load(.duration)
+    return CMTimeGetSeconds(d)
+  }
+}
--- DanceMachine/DanceMachine/Presentations/Home/Views/VideoList/VideoPreview.swift
+++ DanceMachine/DanceMachine/Presentations/Home/Views/VideoList/VideoPreview.swift
@@ -10,13 +10,17 @@ import AVKit
 import Photos
 
 struct VideoPreview: View {
-  @State private var player: AVPlayer?
+//  @State private var player: AVPlayer?
   @State private var isLoading: Bool = false
   
-  @Binding var selectedAsset: PHAsset?
+  @Bindable var vm: VideoPickerVM
+  
+//  @Binding var selectedAsset: PHAsset?
+  
+  var size: CGFloat
   
 //  let asset: PHAsset
-  let onConfirm: (URL, UIImage, Double) -> Void
+//  let onConfirm: (URL, UIImage, Double) -> Void
   
   var body: some View {
     
@@ -43,28 +47,33 @@ struct VideoPreview: View {
 }
   
   private var realBody: some View {
-    ZStack {
-//      Color.black.opacity(0.5)
-//        .ignoresSafeArea(edges: .all)
-//      
+    VStack {
+      
       if isLoading {
         loadingView
-      } else if let p = player {
+          .frame(height: size)
+      } else if let p = vm.player {
         VideoPlayer(player: p)
           .aspectRatio(16/9, contentMode: .fit)
+          .frame(height: size)
         //          .onAppear {
         //            p.play()
         //          }
       } else {
-        Text("비디오를 선택해 주세요.")
-          .foregroundStyle(.white)
+        VStack {
+          Text("비디오를 선택해 주세요.")
+            .foregroundStyle(.black)
+            .multilineTextAlignment(.center)
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: size)
       }
     }
-    .onChange(of: selectedAsset, { oldValue, newValue in
+    .onChange(of: vm.selectedAsset, { oldValue, newValue in
       if newValue != nil {
-        self.loadVideo()
+        vm.loadVideo()
       } else {
-        self.player = nil
+        vm.player = nil
       }
     })
     .ignoresSafeArea()
@@ -81,93 +90,92 @@ struct VideoPreview: View {
     }
   }
   
-  private func loadVideo() {
-    self.isLoading = true
+//  private func loadVideo() {
+//    self.isLoading = true
+//    
+//    let options = PHVideoRequestOptions()
+//    options.isNetworkAccessAllowed = true
+//    
+//    PHImageManager.default().requestPlayerItem(
+//      forVideo: selectedAsset ?? PHAsset(),
+//      options: options) { item, _ in
+//        DispatchQueue.main.async {
+//          if let pItem = item {
+//            self.player = AVPlayer(playerItem: pItem)
+//            self.isLoading = false
+//          }
+//        }
+//      }
+//  }
+  
+//  private func exportVideo() {
+//    self.isLoading = true
 //    defer { self.isLoading = false }
-    
-    let options = PHVideoRequestOptions()
-    options.isNetworkAccessAllowed = true
-    
-    PHImageManager.default().requestPlayerItem(
-      forVideo: selectedAsset ?? PHAsset(),
-      options: options) { item, _ in
-        DispatchQueue.main.async {
-          if let pItem = item {
-            self.player = AVPlayer(playerItem: pItem)
-            self.isLoading = false
-          }
-        }
-      }
-  }
+//    
+//    let o = PHVideoRequestOptions()
+//    o.isNetworkAccessAllowed = true
+//    o.deliveryMode = .highQualityFormat
+//    
+//    PHImageManager.default().requestAVAsset(
+//      forVideo: selectedAsset ?? PHAsset(),
+//      options: o) { avAsset, _, _ in
+//        guard let urlAsset = avAsset as? AVURLAsset else { return }
+//        
+//        Task {
+//          // 임시 폴더로 복사
+//          let fileName = "video_\(UUID().uuidString).mov"
+//          let tempURL = URL.temporaryDirectory.appending(component: fileName)
+//          
+//          do {
+//            // 파일 복사
+//            if FileManager.default.fileExists(atPath: tempURL.path()) {
+//              try FileManager.default.removeItem(at: tempURL)
+//              print("\(tempURL.path()) 임시 디렉토리 삭제")
+//            }
+//            try FileManager.default.copyItem(at: urlAsset.url, to: tempURL)
+//            print("\(tempURL.path()) 임시 디렉토리 추가")
+//            
+//            async let t = generateThumbnail(from: urlAsset)
+//            async let d = getDuration(from: urlAsset)
+//            
+//            let (thumbnail, duration) = try await (t, d)
+//            
+//            await MainActor.run {
+//              onConfirm(tempURL, thumbnail ?? UIImage(), duration)
+//              isLoading = false
+//              
+//            }
+//          } catch {
+//            print("비디오 복사 실패")
+//          }
+//        }
+//      }
+//  }
   
-  private func exportVideo() {
-    self.isLoading = true
-    defer { self.isLoading = false }
-    
-    let o = PHVideoRequestOptions()
-    o.isNetworkAccessAllowed = true
-    o.deliveryMode = .highQualityFormat
-    
-    PHImageManager.default().requestAVAsset(
-      forVideo: selectedAsset ?? PHAsset(),
-      options: o) { avAsset, _, _ in
-        guard let urlAsset = avAsset as? AVURLAsset else { return }
-        
-        Task {
-          // 임시 폴더로 복사
-          let fileName = "video_\(UUID().uuidString).mov"
-          let tempURL = URL.temporaryDirectory.appending(component: fileName)
-          
-          do {
-            // 파일 복사
-            if FileManager.default.fileExists(atPath: tempURL.path()) {
-              try FileManager.default.removeItem(at: tempURL)
-              print("\(tempURL.path()) 임시 디렉토리 삭제")
-            }
-            try FileManager.default.copyItem(at: urlAsset.url, to: tempURL)
-            print("\(tempURL.path()) 임시 디렉토리 추가")
-            
-            async let t = generateThumbnail(from: urlAsset)
-            async let d = getDuration(from: urlAsset)
-            
-            let (thumbnail, duration) = try await (t, d)
-            
-            await MainActor.run {
-              onConfirm(tempURL, thumbnail ?? UIImage(), duration)
-              isLoading = false
-              
-            }
-          } catch {
-            print("비디오 복사 실패")
-          }
-        }
-      }
-  }
+//  private func generateThumbnail(
+//    from asset: AVURLAsset
+//  ) async throws-> UIImage? {
+//    
+//    let imageG = AVAssetImageGenerator(asset: asset)
+//    imageG.appliesPreferredTrackTransform = true
+//    
+//    let t = CMTime(seconds: 1, preferredTimescale: 60)
+//    
+//    let (cgImage, _) = try await imageG.image(at: t)
+//    return UIImage(cgImage: cgImage)
+//  }
   
-  private func generateThumbnail(
-    from asset: AVURLAsset
-  ) async throws-> UIImage? {
-    
-    let imageG = AVAssetImageGenerator(asset: asset)
-    imageG.appliesPreferredTrackTransform = true
-    
-    let t = CMTime(seconds: 1, preferredTimescale: 60)
-    
-    let (cgImage, _) = try await imageG.image(at: t)
-    return UIImage(cgImage: cgImage)
-  }
-  
-  private func getDuration(
-    from asset: AVURLAsset
-  ) async throws -> Double {
-    
-    let d = try await asset.load(.duration)
-    return CMTimeGetSeconds(d)
-  }
+//  private func getDuration(
+//    from asset: AVURLAsset
+//  ) async throws -> Double {
+//    
+//    let d = try await asset.load(.duration)
+//    return CMTimeGetSeconds(d)
+//  }
 }
 
-#Preview {
-  NavigationStack {
-    VideoPreview(selectedAsset: .constant(PHAsset()), onConfirm: { _, _, _ in })
-  }
-}
+//#Preview {
+//  NavigationStack {
+//    VideoPreview(selectedAsset: .constant(PHAsset()), size: 100, onConfirm: { _, _, _ in })
+//  }
+//}
