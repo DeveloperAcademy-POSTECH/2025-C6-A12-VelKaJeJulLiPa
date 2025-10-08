e4c804474f63752359324e704573a0f41902067e
Velko <wlsgurrla716@naver.com>
2025-10-08 14:30:04 +0900

    ✨[Feat]#6 - HomeView 팀 스페이스 타이틀 타입 구현

---
+++ DanceMachine/DanceMachine/Common/Enum/Home/TeamspaceRoute.swift
@@ -0,0 +1,13 @@
+//
+//  TeamspaceRoute.swift
+//  DanceMachine
+//
+//  Created by 김진혁 on 10/8/25.
+//
+
+import Foundation
+
+enum TeamspaceRoute {
+    case createTeamSpace     // 팀 스페이스 없음 → 생성 화면(5)
+    case showTeamSpaceList   // 팀 스페이스 있음 → 목록 화면(4)
+}
