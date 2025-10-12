29a9ea49d2f6d679ad3df7b12d00f15f2f3c3e7a
Paidion <dev.paidion@gmail.com>
2025-10-13 00:17:59 +0900

    ✨[Feat]#6 - DB에 사용자 정보 저장
    - Firebase Authentication의 사용자 정보로 DB 에 사용자 정보 저장
    - DB에 사용자 정보가 있다면, 다시 로그인할 때는 최근 로그인 시각만 저장

--- DanceMachine/DanceMachine/Presentations/Login/ViewModels/LoginViewModel.swift
+++ DanceMachine/DanceMachine/Presentations/Login/ViewModels/LoginViewModel.swift
@@ -7,21 +7,24 @@
 
 import Foundation
 import AuthenticationServices
+import FirebaseAuth
 
+
+@MainActor
 final class LoginViewModel {
     
-    private let authManager = FirebaseAuthManager.shared
-    
-    func handleSignInWithAppleRequest(_ request: ASAuthorizationAppleIDRequest) {
-        authManager.handleSignInWithAppleRequest(request)
+    func signInApple() async throws {
+        let helper = SignInAppleHelper()
+        let tokens = try await helper.startSignInWithAppleFlow()
+        let authDataResult = try await FirebaseAuthManager.shared.signInWithApple(tokens: tokens)
+        await FirebaseAuthManager.shared.updateDisplayName(for: authDataResult.user, with: tokens.name)
+        let user = User(user: authDataResult.user)
+        
+        // Check whether user document exists or not
+        if let user: User = try? await FirestoreManager.shared.get(user.userId, from: .users) {
+            try await FirestoreManager.shared.updateUserLastLogin(user)
+        } else {
+            try await FirestoreManager.shared.createUser(user)
+        }
     }
-
-    func handleSignInWithAppleCompletion(_ result: Result<ASAuthorization, Error>) {
-        authManager.handleSignInWithAppleCompletion(result)
-    }
-
 }
-
-    
-
-
