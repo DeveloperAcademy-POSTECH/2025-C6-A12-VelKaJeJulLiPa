c7def6206716455fb4717fb921a74baeb48c531e
Paidion <dev.paidion@gmail.com>
2025-10-13 00:22:35 +0900

    🔧[Chore]#4 - 애플 로그인 버튼 수정
    - LoginViewModel 에서 애플 로그인 로직 처리할 수 있도록 수정

--- DanceMachine/DanceMachine/Presentations/Login/Views/LoginView.swift
+++ DanceMachine/DanceMachine/Presentations/Login/Views/LoginView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 import AuthenticationServices
 
+// FIXME: Implement Hi-fi Design
 struct LoginView: View {
-    
     @Environment(\.colorScheme) var colorScheme
     @State private var viewModel = LoginViewModel()
     
@@ -21,11 +21,21 @@ struct LoginView: View {
                 .font(.largeTitle.bold())
                 .multilineTextAlignment(.center)
             
-            SignInWithAppleButton(
-                onRequest: viewModel.handleSignInWithAppleRequest,
-                onCompletion: viewModel.handleSignInWithAppleCompletion
-            )
-            .signInWithAppleButtonStyle(colorScheme == .light ? .black : .white)
+            Button(action: {
+                Task {
+                    do {
+                        try await viewModel.signInApple()
+                    } catch {
+                        print(error)
+                    }
+                }
+            }, label: {
+                SignInWithAppleButtonViewRepresentable(
+                    type: .default,
+                    style: colorScheme == .light ? .black : .white
+                )
+                .allowsHitTesting(false)
+            })
             .frame(height: 50)
             .cornerRadius(8)
             .padding(.horizontal, 32)
