4f9e12644f8359d40fb80af45c2766bbd623b144
Paidion <dev.paidion@gmail.com>
2025-10-16 01:17:30 +0900

    ✨[Feat]#4 - 사용자 인증 상태, UserDefaultsKey  enum 생성

---
+++ DanceMachine/DanceMachine/Common/Enum/AuthenticationState.swift
@@ -0,0 +1,13 @@
+//
+//  AuthenticationState.swift
+//  DanceMachine
+//
+//  Created by Paidion on 10/15/25.
+//
+
+import Foundation
+
+enum AuthenticationState {
+    case unauthenticated
+    case authenticated
+}
---
+++ DanceMachine/DanceMachine/Common/Enum/UserDefaultsKey.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultsKey.swift
+//  DanceMachine
+//
+//  Created by Paidion on 10/13/25.
+//
+
+import Foundation
+
+enum UserDefaultsKey: String, CaseIterable {
+    case hasLaunchedBefore = "hasLaunchedBefore"
+    case hasNameSet = "hasNameSet"
+}
