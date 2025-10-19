cbc3fe79e5b3f1ebe3e68d32170fa07a38f1e918
jHoon99 <eldkf1597@naver.com>
2025-10-19 18:06:01 +0900

    ✨[Feat]#10 - NotificationName 지정
    * 원래는 Notification.Name으로 했었는데 파일 오류가 나서 경로를 명시해줌 (이유는 아직 못 찾음)

---
+++ DanceMachine/DanceMachine/Resource/Extension/NotificationName+.swift
@@ -0,0 +1,13 @@
+//
+//  Notification+.swift
+//  DanceMachine
+//
+//  Created by 조재훈 on 10/19/25.
+//
+
+import Foundation
+
+extension Foundation.Notification.Name {
+  static let sectionDidUpdate = Foundation.Notification.Name("sectionDidUpdate")
+}
+
