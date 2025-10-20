c374e182d6045fa82998e1229b280800254d22be
jHoon99 <eldkf1597@naver.com>
2025-10-20 11:14:43 +0900

    🔧[Chore]#10 - 바텀 시트 컴포넌트 추가
    *벨코 구현

---
+++ DanceMachine/DanceMachine/Common/Components/BottomConfirmSheetView.swift
@@ -0,0 +1,35 @@
+//  DanceMachine
+//
+//  Created by 김진혁 on 10/16/25.
+//
+
+import SwiftUI
+
+/// 바텀 시트 뷰
+struct BottomConfirmSheetView: View {
+    let titleText: String
+    let primaryText: String
+    let action: () -> Void
+
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea()
+            BottomConfirmSheet(
+                titleText: titleText,
+                primaryText: primaryText
+            ) { action() }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 33)
+            .presentationDetents([.fraction(0.4)])
+            .presentationCornerRadius(16)
+        }
+    }
+}
+
+#Preview {
+    BottomConfirmSheetView(
+        titleText: "조카단\n팀스페이스를 나가시겠어요?",
+        primaryText: "나가기") {
+
+        }
+}
