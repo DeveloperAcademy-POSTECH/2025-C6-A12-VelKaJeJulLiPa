a87ca05675b3a68962a1576beb6ed85570b6d13f
Paidion <dev.paidion@gmail.com>
2025-10-23 03:19:33 +0900

    ✨[Feat]#11 - 홈화면에서 알림 권한 요청 구현
    - 로그인 전에 알림 권한을 물어보는 것은 무의미하기 떄문에 로그인 이후에 홈화면에 처음 진입했을 때에만 알림 권한을 요청

--- DanceMachine/DanceMachine/Presentations/Home/Views/HomeView.swift
+++ DanceMachine/DanceMachine/Presentations/Home/Views/HomeView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @EnvironmentObject private var router: NavigationRouter
+    
+    @State private var viewModel: HomeViewModel = .init()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+        }.task {
+            await viewModel.setupNotificationAuthorizationIfNeeded()
         }
         .padding()
     }
