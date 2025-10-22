fcddf9a1715f51a115fbd030126c3bd72f576edf
Paidion <dev.paidion@gmail.com>
2025-10-23 03:08:56 +0900

    🔥[Del]#11 - App 파일에 있던 AppDelegate 코드 삭제

--- DanceMachine/DanceMachine/App/DanceMachineApp.swift
+++ DanceMachine/DanceMachine/App/DanceMachineApp.swift
@@ -10,16 +10,6 @@ import FirebaseCore
 import FirebaseFirestore
 
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        print("🔥 FirebaseApp configured")
-        
-        return true
-    }
-}
-
 @main
 struct DanceMachineApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
