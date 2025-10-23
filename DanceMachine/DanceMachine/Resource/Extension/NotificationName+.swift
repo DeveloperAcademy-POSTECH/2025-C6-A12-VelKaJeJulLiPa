5fe50035575a49d1f62bf384d8c3a8e4217e6331
jHoon99 <eldkf1597@naver.com>
2025-10-23 17:17:42 +0900

    ✨[Feat]#10 - 글래스모피즘으로 플로팅 버튼이 rootView에서 관리 될때의 노티기능

--- DanceMachine/DanceMachine/Resource/Extension/NotificationName+.swift
+++ DanceMachine/DanceMachine/Resource/Extension/NotificationName+.swift
@@ -9,5 +9,6 @@ import Foundation
 
 extension Foundation.Notification.Name {
   static let sectionDidUpdate = Foundation.Notification.Name("sectionDidUpdate")
+  static let showVideoPicker = Foundation.Notification.Name("showVideoPicker")
 }
 
