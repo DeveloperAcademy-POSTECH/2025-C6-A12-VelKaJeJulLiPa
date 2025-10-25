f1c3f4290eea64f06bc8e418975ee66fcd71beb8
jHoon99 <eldkf1597@naver.com>
2025-10-25 18:05:27 +0900

    ✨[Feat]#10 - 피드백 관련 로직 뷰모델 구현

--- DanceMachine/DanceMachine/Presentations/Home/ViewModels/VideoPlay/VideoDetailViewModel.swift
+++ DanceMachine/DanceMachine/Presentations/Home/ViewModels/VideoPlay/VideoDetailViewModel.swift
@@ -139,3 +139,35 @@ extension VideoDetailViewModel {
     return vm
   }
 }
+// MARK: 팀 스페이스 관련
+extension VideoDetailViewModel {
+  // 팀 스페이스 멤버 조회
+  func loadTeamMemvers(teamspaceId: String) async {
+    do {
+      let members: [Members] = try await store.fetchAllFromSubcollection(
+        under: .teamspace,
+        parentId: teamspaceId,
+        subCollection: .members
+      )
+      
+      var users: [User] = []
+      for member in members {
+        let user: [User] = try await store.get(
+          member.userId,
+          from: .users
+        )
+        users.append(contentsOf: user)
+      }
+      await MainActor.run {
+        self.teamMembers = users
+      }
+    } catch { // TODO: 에러 처리
+      print("팀 멤버 조회 실패: \(error)")
+    }
+  }
+  // 멘션 검색 기능
+  func searchMembers(query: String) -> [User] {
+    guard !query.isEmpty else { return teamMembers }
+    return teamMembers.filter { $0.name.lowercased().contains(query.lowercased()) }
+  }
+}
