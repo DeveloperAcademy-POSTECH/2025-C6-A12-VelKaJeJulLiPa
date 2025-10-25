ee73660bbbfecaf5e51100697086e1c88291ec65
jHoon99 <eldkf1597@naver.com>
2025-10-25 21:44:52 +0900

    ✨[Feat]#10 - 피드백 리스트 videoView와 합치는 작업

--- DanceMachine/DanceMachine/Presentations/Home/Views/VideoView/VideoView.swift
+++ DanceMachine/DanceMachine/Presentations/Home/Views/VideoView/VideoView.swift
@@ -58,8 +58,73 @@ struct VideoView: View {
       }
     }
   }
+  // MARK: 피드백 리스트
+  private var feedbackListView: some View {
+    ZStack {
+      Image(systemName: "circle")
+        .resizable()
+        .frame(width: 100)
+        .frame(height: 100)
+      ScrollView {
+        LazyVStack {
+          //        if vm.feedbackVM.feedbacks.isEmpty {
+          //          emptyView
+          //        } else {
+          ForEach(vm.feedbackVM.feedbacks, id: \.feedbackId) { f in
+            FeedbackCard(
+              feedback: f,
+              taggedUsers:
+                vm.getTaggedUsers(for: f.taggedUserIds),
+              replyCount: vm.feedbackVM.reply[f.feedbackId.uuidString]?.count ?? 0,
+              action: { self.showReplyModal = true }
+            )
+          }
+        }
+        .padding(.horizontal, 16)
+      }
+    }
+  }
+  // MARK: 피드백 섹션
+  private var feedbackSection: some View {
+    HStack {
+      Text("전체 피드백")
+      Spacer()
+      Button {
+        // TODO: 피드백 필터링 기능
+      } label: { // FIXME: 디자인 수정
+        Text("마이 피드백")
+          .foregroundStyle(Color.white)
+          .padding(.horizontal, 11)
+          .padding(.vertical, 7)
+          .background(
+            RoundedRectangle(cornerRadius: 10)
+              .fill(Color.gray)
+              .stroke(Color.white)
+          )
+      }
+    }
+    .padding(.horizontal, 16)
+  }
+  
+//  private var emptyView: some View {
+//    VStack(spacing: 16) {
+//      Spacer()
+//      Image(systemName: "bubble.left.and.bubble.right")
+//        .font(.system(size: 48))
+//        .foregroundStyle(.gray)
+//      Text("피드백이 없습니다")
+//        .font(.headline)
+//        .foregroundStyle(.gray)
+//      Spacer()
+//    }
+//    .frame(maxWidth: .infinity)
+//  }
 }
 
 #Preview {
-  VideoView(videoURL: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8")
+  @Previewable @State var vm: VideoDetailViewModel = .preview
+  NavigationStack {
+    VideoView(vm: vm, videoTitle: "벨코의 위플래시")
+  }
+  .environmentObject(NavigationRouter())
 }
