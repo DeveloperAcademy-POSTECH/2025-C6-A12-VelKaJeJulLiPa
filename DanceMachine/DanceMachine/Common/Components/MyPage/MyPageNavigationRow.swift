36bc367cf791eb98ba2bbe9e09254589bc5f2517
Paidion <dev.paidion@gmail.com>
2025-10-25 19:57:29 +0900

    ✨[Feat]#12 - 마이페이지 컴포넌틑 추가

---
+++ DanceMachine/DanceMachine/Common/Components/MyPage/MyPageNavigationRow.swift
@@ -0,0 +1,43 @@
+//
+//  MyPageNavigationRow.swift
+//  DanceMachine
+//
+//  Created by Paidion on 10/25/25.
+//
+
+import SwiftUI
+
+/// 마이페이지에서 정보를 보여줄 수 있고 네비게이션이 있는 행
+struct MyPageNavigationRow: View {  // FIXME: - Hi-fi 스타일 적용
+    let title: String
+    var value: String? = nil
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            HStack {
+                Text(title)
+                    .font(.system(size: 16))
+                    .foregroundColor(.primary)
+                
+                Spacer()
+                
+                if let value = value {
+                    Text(value)
+                        .font(.system(size: 16))
+                        .foregroundColor(.secondary)
+                        .padding(.trailing, 4)
+                }
+                
+                Image(systemName: "chevron.right")
+                    .font(.system(size: 14, weight: .semibold))
+                    .foregroundColor(.secondary)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    MyPageNavigationRow(title: "TItle", value: "value", action: { print("Click") })
+}
