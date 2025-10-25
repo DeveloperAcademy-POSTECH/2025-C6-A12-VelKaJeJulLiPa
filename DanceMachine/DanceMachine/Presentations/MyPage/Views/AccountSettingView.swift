e3f9a7665cf80fdb6fb93fc77d155eea5d0ab1d1
Paidion <dev.paidion@gmail.com>
2025-10-25 20:10:46 +0900

    ✨[Feat]#12 - 계정 설정 화면 구현

---
+++ DanceMachine/DanceMachine/Presentations/MyPage/Views/AccountSettingView.swift
@@ -0,0 +1,84 @@
+//
+//  AccountSettingView.swift
+//  DanceMachine
+//
+//  Created by Paidion on 10/25/25.
+//
+
+import SwiftUI
+
+struct AccountSettingView: View {
+    
+    @EnvironmentObject private var router: NavigationRouter
+    
+    @State private var viewModel = AccountSettingViewModel()
+    
+    @State private var isPresentingSignOutSheet: Bool = false     // 로그아웃 시트 제어
+    @State private var isPresentingDeleteUserSheet: Bool = false  // 회원탈퇴 시트 제어
+    
+    
+    var body: some View {
+        ZStack {
+            Color(UIColor.systemGroupedBackground) // FIXME: - 컬러 수정
+                .ignoresSafeArea()
+            
+            VStack {
+                MyPageInfoRow(
+                    title: "ID",
+                    value: viewModel.myId
+                )
+                
+                Spacer()
+                
+                ActionButton(
+                    title: "로그아웃",
+                    color: Color.orange, // FIXME: - 컬러 수정
+                    height: 47,
+                    action: { isPresentingSignOutSheet = true }
+                )
+                .padding()
+                
+                Button {
+                    isPresentingDeleteUserSheet = true
+                } label: {
+                    Text("회원탈퇴")
+                        .foregroundStyle(.red) // FIXME: - 컬러 수정
+                }
+            }
+        }
+        .sheet(isPresented: $isPresentingSignOutSheet) {
+            BottomConfirmSheetView(
+                titleText: "로그아웃하시겠어요?",
+                primaryText: "로그아웃") {
+                    Task {
+                        try viewModel.signOut()
+                        router.popToRootView()
+                    }
+                }
+        }
+        .sheet(isPresented: $isPresentingDeleteUserSheet) {
+            BottomConfirmSheetView(
+                titleText: "탈퇴하시겠어요?\n모든 정보가 삭제됩니다.",
+                primaryText: "탈퇴") {
+                    Task {
+                        try await viewModel.deleteUserAccount()
+                        router.popToRootView() // FIXME: - 내비게이션 처리 확인
+                    }
+                }
+        }
+        .toolbar {
+            ToolbarLeadingBackButton()
+            ToolbarCenterTitle(text: "계정 설정")
+        }
+        
+    }
+}
+
+
+
+#Preview {
+    AccountSettingView()
+}
+
+
+
