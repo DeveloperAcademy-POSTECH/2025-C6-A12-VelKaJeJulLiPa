8da121fe16832ca79dd02dfb16bf23d242038309
jHoon99 <eldkf1597@naver.com>
2025-10-27 04:42:03 +0900

    ✨[Feat]#10 - 네비게이션 라우팅 연결

--- DanceMachine/DanceMachine/Navigation/NavigationRoutingView.swift
+++ DanceMachine/DanceMachine/Navigation/NavigationRoutingView.swift
@@ -63,6 +63,14 @@ struct NavigationRoutingView: View {
                     trackName: trackName,
                     sectionId: sectionId
                   )
+                case .play(let videoId, let videoTitle, let videoURL, let teamspaceId, let authorId):
+                  VideoView(
+                    teamspaceId: teamspaceId,
+                    authorId: authorId,
+                    videoId: videoId,
+                    videoTitle: videoTitle,
+                    videoURL: videoURL
+                  )
                 }
             }
         }
