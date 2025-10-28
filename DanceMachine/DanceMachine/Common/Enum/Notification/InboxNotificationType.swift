13bfcce678e6904f3cf1ffcfefe2dff5b3d973f9
Paidion <dev.paidion@gmail.com>
2025-10-29 02:38:47 +0900

    ✨[Feat]#11 - 알림 유형 추가

---
+++ DanceMachine/DanceMachine/Common/Enum/Notification/InboxNotificationType.swift
@@ -0,0 +1,14 @@
+//
+//  InboxNotificationType.swift
+//  DanceMachine
+//
+//  Created by Paidion on 10/28/25.
+//
+
+import Foundation
+
+
+enum InboxNotificationType {
+    case feedback
+    case reply
+}
