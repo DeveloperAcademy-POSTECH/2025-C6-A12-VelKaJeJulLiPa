745bf387eae798c9c8094cd734ad726bd17a17cd
Paidion <dev.paidion@gmail.com>
2025-10-30 06:00:14 +0900

    ✨[Feat]#11 - 딥링크 관련 알림(이벤트) 추가
    - 앱이 포그라운드에서 푸시 알림으로 특정 화면으로 이동할 떄 사용됩니다.

--- DanceMachine/DanceMachine/Resource/Extension/NotificationName+.swift
+++ DanceMachine/DanceMachine/Resource/Extension/NotificationName+.swift
@@ -11,18 +11,21 @@ extension Foundation.Notification.Name {
   static let sectionDidUpdate = Foundation.Notification.Name("sectionDidUpdate")
   static let showVideoPicker = Foundation.Notification.Name("showVideoPicker")
   static let videoUpload = Foundation.Notification.Name("videoUpload")
+  static let didReceiveDeeplink = Foundation.Notification.Name("didReceiveDeeplink")
 }
 
 enum NotificationEvent {
   case sectionDidUpdate
   case showVideoPicker
   case videoUpload
-  
+  case didReceiveDeeplink
+ 
   var name: Foundation.Notification.Name {
     switch self {
     case .sectionDidUpdate: .sectionDidUpdate
     case .showVideoPicker: .showVideoPicker
     case .videoUpload: .videoUpload
+    case .didReceiveDeeplink: .didReceiveDeeplink
     }
   }
 }
