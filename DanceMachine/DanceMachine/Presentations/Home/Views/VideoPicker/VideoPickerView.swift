b6741e4a967b9c8e3b8f1dbcf153a09892eef4a7
jHoon99 <eldkf1597@naver.com>
2025-10-31 05:41:58 +0900

    🔧[Chore]#32 - UX개선사항
    * 업로드할때 영상 선택, 영상 이름 등에 대한 ui 처리

--- DanceMachine/DanceMachine/Presentations/Home/Views/VideoPicker/VideoPickerView.swift
+++ DanceMachine/DanceMachine/Presentations/Home/Views/VideoPicker/VideoPickerView.swift
@@ -14,6 +14,9 @@ struct VideoPickerView: View {
   
   @State private var vm: VideoPickerViewModel = .init()
   
+  @State private var showEmptyTitleAlert: Bool = false
+  @State private var showEmptyVideoAlert: Bool = false
+  
   @FocusState private var isFocused: Bool
   
   let tracksId: String
@@ -60,14 +63,21 @@ struct VideoPickerView: View {
           ToolbarCenterTitle(text: "비디오 선택")
           ToolbarItemGroup(placement: .topBarTrailing) {
             Button {
+              if vm.selectedAsset == nil {
+                self.showEmptyVideoAlert = true
+                return
+              } else if vm.videoTitle == "" {
+                self.showEmptyTitleAlert = true
+                return
+              }
               vm.exportVideo(tracksId: tracksId, sectionId: sectionId)
             } label: {
               Image(systemName: "arrow.up")
                 .foregroundStyle(
-                  vm.selectedAsset == nil ? .purple.opacity(0.7) : .white
+                  vm.selectedAsset == nil ? .black.opacity(0.7) : .white
                 )
             }
-            .disabled(vm.selectedAsset == nil || vm.isLoading) // 이중 비활성
+            .disabled(vm.isLoading) // 이중 비활성
             .buttonStyle(.borderedProminent)
             .tint(.blue)
           }
@@ -75,18 +85,22 @@ struct VideoPickerView: View {
         .task {
           await vm.requestPermissionAndFetch()
         }
-        .alert(
-          "업로드 성공",
-          isPresented: $vm.showSuccessAlert
-        ) {
+        .alert("비디오를 선택해주세요!", isPresented: $showEmptyVideoAlert) {
+          Button("확인") {
+            self.showEmptyVideoAlert = false
+          }
+        }
+        .alert("파일명을 입력하세요!", isPresented: $showEmptyTitleAlert) {
+          Button("확인") {
+            self.showEmptyTitleAlert = false
+          }
+        }
+        .alert("업로드 완료", isPresented: $vm.showSuccessAlert) {
           Button("확인") {
             dismiss()
           }
         }
-        .alert(
-          "업로드 실패",
-          isPresented: .constant(vm.errorMessage != nil)
-        ) {
+        .alert("업로드 실패", isPresented: .constant(vm.errorMessage != nil)) {
           Button("확인") {
             vm.errorMessage = nil
           }
@@ -102,23 +116,25 @@ struct VideoPickerView: View {
             VStack(spacing: 16) {
               ProgressView()
                 .progressViewStyle(CircularProgressViewStyle())
-                .tint(.purple)
-                .scaleEffect(2)
+                .tint(.black)
+                .scaleEffect(1)
               
               if vm.uploadProgress > 0 {
                 Text("업로드 중... \(Int(vm.uploadProgress * 100))%")
                   .foregroundStyle(.purple)
-                  .font(.system(size: 20))
+                  .font(.system(size: 14))
               } else {
                 Text("준비 중...")
                   .foregroundStyle(.purple)
-                  .font(.system(size: 20))
+                  .font(.system(size: 14))
               }
             }
           }
+          .ignoresSafeArea()
         }
       }
     }
+    .background(Color.white) // FIXME: 다크모드 배경색 명시
     .disabled(vm.isLoading)
   }
   
@@ -128,7 +144,7 @@ struct VideoPickerView: View {
       .frame(maxWidth: .infinity)
       .frame(height: 51)
       .overlay {
-        TextField("업로드할 영상을 선택하세요.", text: $vm.videoTitle)
+        TextField("파일명을 입력하세요.", text: $vm.videoTitle)
           .padding()
           .textFieldStyle(.plain)
           .font(.system(size: 16)) // FIXME: 폰트 수정
