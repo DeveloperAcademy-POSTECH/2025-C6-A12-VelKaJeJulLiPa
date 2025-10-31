b3c795a5b17fa73f60929f321e3458b54d1a0f31
Velko <wlsgurrla716@naver.com>
2025-10-31 17:12:51 +0900

    🐛[Fix] - iOS 18에서 리스트 터치가 안되는 버그를 수정함
    .simultaneousGesture(
              TapGesture()
                .onEnded {}
    )

--- DanceMachine/DanceMachine/Presentations/Home/Views/TeamspaceView/TeamspaceListView.swift
+++ DanceMachine/DanceMachine/Presentations/Home/Views/TeamspaceView/TeamspaceListView.swift
@@ -8,70 +8,71 @@
 import SwiftUI
 
 struct TeamspaceListView: View {
-    
-    @EnvironmentObject private var router: NavigationRouter
-    
-    @State private var viewModel: TeamspaceListViewModel = .init()
-    
-    @State private var userTeamspaces: [UserTeamspace] = []
-    @State private var loadTeamspaces: [Teamspace] = []
-    
-    @State private var isLoading: Bool = false
-    
-    // 프리뷰·테스트용 주입 이니셜라이저
-    init(previewLoadTeamspaces: [Teamspace] = []) {
-        _loadTeamspaces = State(initialValue: previewLoadTeamspaces)
-    }
-      
-    var body: some View {
-        ZStack {
-            Color.white.ignoresSafeArea() // FIXME: - 컬러 수정
-            List {
-                ForEach(loadTeamspaces, id: \.teamspaceId) { teamspace in
-                    Button {
-                        print("탭탭")
-                        viewModel.fetchCurrentTeamspace(teamspace: teamspace)
-                        router.pop()
-                    } label: {
-                        TeamspaceListItem(title: teamspace.teamspaceName)
-                    }
-                    .buttonStyle(.plain)
-                    .listRowSeparator(.hidden)
-                    .listRowBackground(Color.white)
-                }
-                
-                Button {
-                    router.push(to: .teamspace(.create))
-                } label: {
-                    TeamspaceListItem(title: "+ 팀 스페이스 만들기")
-                }
-                .buttonStyle(.plain)
-                .listRowSeparator(.hidden)
-                .listRowBackground(Color.white)
+  
+  @EnvironmentObject private var router: NavigationRouter
+  
+  @State private var viewModel: TeamspaceListViewModel = .init()
+  
+  @State private var userTeamspaces: [UserTeamspace] = []
+  @State private var loadTeamspaces: [Teamspace] = []
+  
+  @State private var isLoading: Bool = false
+  
+  // 프리뷰·테스트용 주입 이니셜라이저
+  init(previewLoadTeamspaces: [Teamspace] = []) {
+    _loadTeamspaces = State(initialValue: previewLoadTeamspaces)
+  }
+  
+  var body: some View {
+    ZStack {
+      Color.white.ignoresSafeArea() // FIXME: - 컬러 수정
+      List {
+        ForEach(loadTeamspaces, id: \.teamspaceId) { teamspace in
+          TeamspaceListItem(title: teamspace.teamspaceName)
+            .simultaneousGesture(
+              TapGesture().onEnded {
+                viewModel.fetchCurrentTeamspace(teamspace: teamspace)
+                router.pop()
+              }
+            )
+            .buttonStyle(.plain)
+            .listRowSeparator(.hidden)
+            .listRowBackground(Color.white)
+        }
+
+        TeamspaceListItem(title: "+ 팀 스페이스 만들기")
+          .simultaneousGesture(
+            TapGesture().onEnded {
+              router.push(to: .teamspace(.create))
             }
-            .listStyle(.plain)
-            .scrollContentBackground(.hidden)
-        }
-        .overlay { if isLoading { LoadingView() } }
-        .toolbar {
-            ToolbarLeadingBackButton(icon: .chevron)
-            ToolbarCenterTitle(text: "팀 스페이스")
-        }
-        .task {
-            if ProcessInfo.isRunningInPreviews { return } // 프리뷰 전용
-            self.isLoading = true
-            defer { isLoading = false }
-            
-            let loaded: [Teamspace] = (try? await viewModel.loadTeamspacesForUser()) ?? []
-            self.loadTeamspaces = loaded
-        }
+          )
+          .buttonStyle(.plain)
+          .listRowSeparator(.hidden)
+          .listRowBackground(Color.white)
+      }
+      .listStyle(.plain)
+      .scrollContentBackground(.hidden)
     }
+    .overlay { if isLoading { LoadingView() } }
+    .toolbar {
+      ToolbarLeadingBackButton(icon: .chevron)
+      ToolbarCenterTitle(text: "팀 스페이스")
+    }
+    .task {
+      if ProcessInfo.isRunningInPreviews { return } // 프리뷰 전용
+      self.isLoading = true
+      defer { isLoading = false }
+      
+      let loaded: [Teamspace] = (try? await viewModel.loadTeamspacesForUser()) ?? []
+      self.loadTeamspaces = loaded
+    }
+  }
 }
 
 #Preview {
-    NavigationStack {
-        TeamspaceListView(previewLoadTeamspaces: Teamspace.TeamspaceMockData)
-            .environmentObject(NavigationRouter())
-    }
+  NavigationStack {
+    TeamspaceListView(previewLoadTeamspaces: Teamspace.TeamspaceMockData)
+      .environmentObject(NavigationRouter())
+  }
 }
 
