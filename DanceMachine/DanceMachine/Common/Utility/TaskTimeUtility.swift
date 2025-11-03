e14e81852bb4988414e224456c51c3102d03be1d
jHoon99 <eldkf1597@naver.com>
2025-11-04 02:26:38 +0900

    ✨[Feat]#37 - 스켈레톤 뷰 뜨는 시간 명시해주는 유틸리티 함수

---
+++ DanceMachine/DanceMachine/Common/Utility/TaskTimeUtility.swift
@@ -0,0 +1,21 @@
+//
+//  TaskTimeUtility.swift
+//  DanceMachine
+//
+//  Created by 조재훈 on 11/3/25.
+//
+
+import Foundation
+
+/// 스켈레톤 뷰 뜨는 시간을 보장해주는 유틸리티 구조체 입니다.
+struct TaskTimeUtility {
+  static func waitForMinimumLoadingTime(
+    startTime: Date,
+    minimim: TimeInterval = 1.5
+  ) async {
+    let elapedTime = Date().timeIntervalSince(startTime)
+    if elapedTime < minimim {
+      try? await Task.sleep(nanoseconds: UInt64(minimim - elapedTime) * 1_000_000_000)
+    }
+  }
+}
