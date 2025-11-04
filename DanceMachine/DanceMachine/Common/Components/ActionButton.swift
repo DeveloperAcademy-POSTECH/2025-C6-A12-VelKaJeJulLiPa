81af4a100145d5c4a64880689f1b6ab45ebf84ab
Paidion <dev.paidion@gmail.com>
2025-11-05 04:14:25 +0900

    💄[Design]#46 - ActionButton Hi-fi 반영

--- DanceMachine/DanceMachine/Common/Components/ActionButton.swift
+++ DanceMachine/DanceMachine/Common/Components/ActionButton.swift
@@ -14,53 +14,53 @@ import SwiftUI
 ///     - height: 버튼의 높이
 ///     - action: 버튼의 액션
 struct ActionButton: View {
-    let title: String
-    let color: Color
-    let height: CGFloat
-    var isEnabled: Bool = true
-    let action: () -> Void
-    
-    init(
-        title: String,
-        color: Color,
-        height: CGFloat,
-        isEnabled: Bool = true,
-        action: @escaping () -> Void
-    ) {
-        self.title = title
-        self.color = color
-        self.height = height
-        self.isEnabled = isEnabled
-        self.action = action
-    }
-    
-    var body: some View {
-        Button {
-            action()
-        } label: {
-            RoundedRectangle(cornerRadius: 5)
-                .fill(color)
-                .overlay {
-                    Text(title)
-                        .font(Font.system(size: 16, weight: .medium)) // FIXME: - 폰트 수정
-                        .foregroundStyle(Color.white) // FIXME: - 컬러 수정
-                }
+  let title: String
+  let color: Color
+  let height: CGFloat
+  var isEnabled: Bool = true
+  let action: () -> Void
+  
+  init(
+    title: String,
+    color: Color,
+    height: CGFloat,
+    isEnabled: Bool = true,
+    action: @escaping () -> Void
+  ) {
+    self.title = title
+    self.color = color
+    self.height = height
+    self.isEnabled = isEnabled
+    self.action = action
+  }
+  
+  var body: some View {
+    Button {
+      action()
+    } label: {
+      RoundedRectangle(cornerRadius: 15)
+        .fill(isEnabled ? color : .fillAssitive)
+        .overlay {
+          Text(title)
+            .font(.headline2Medium)
+            .foregroundStyle(isEnabled ? .labelStrong : .labelAssitive)
         }
-        .frame(maxWidth: .infinity)
-        .frame(height: height)
-        .disabled(!isEnabled)
     }
+    .frame(maxWidth: .infinity)
+    .frame(height: height)
+    .disabled(!isEnabled)
+  }
 }
 
 #Preview(traits: .sizeThatFitsLayout) {
-    VStack {
-        ActionButton(
-            title: "메인 액션",
-            color: Color.blue,
-            height: 47
-        ) {
-            
-        }
+  VStack {
+    ActionButton(
+      title: "메인 액션",
+      color: Color.blue,
+      height: 47
+    ) {
+      
     }
-    .padding(.horizontal, 16)
+  }
+  .padding(.horizontal, 16)
 }
