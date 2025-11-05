0b6b70a9b46347e07a16a1f71aed9a7b81f5e2c4
Paidion <dev.paidion@gmail.com>
2025-11-06 03:58:52 +0900

    ✨[Feat]#46 - user_notification 콜렉션의 created_at 필드 프로퍼티 추가
    - 서버에 쿼리를 보낼 때 필요해서 추가함

--- DanceMachine/DanceMachine/Models/DTO/SubCollection/UserNotification.swift
+++ DanceMachine/DanceMachine/Models/DTO/SubCollection/UserNotification.swift
@@ -5,21 +5,31 @@
 //  Created by Paidion on 10/30/25.
 //
 
+import Foundation
+
 struct UserNotification: Codable, Equatable, Hashable {
   let notificationId: String
   let teamspaceId: String
   let isRead: Bool
+  let createdAt: Date?
   
-  init(notificationId: String, teamspaceId: String, isRead: Bool) {
+  init(
+    notificationId: String,
+    teamspaceId: String,
+    isRead: Bool,
+    createdAt: Date? = nil,
+  ) {
     self.notificationId = notificationId
     self.teamspaceId = teamspaceId
     self.isRead = isRead
+    self.createdAt = createdAt
   }
   
   enum CodingKeys: String, CodingKey {
     case notificationId = "notification_id"
     case teamspaceId = "teamspace_id"
     case isRead = "is_read"
+    case createdAt = "created_at"
   }
 }
 
