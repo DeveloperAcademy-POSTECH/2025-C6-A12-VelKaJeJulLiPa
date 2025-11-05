4f321ac0e61ef092e34a89fe94608d7c5b7b865c
Velko <wlsgurrla716@naver.com>
2025-11-05 23:37:23 +0900

    ✨[Feat]#42 - 토스트 메세지 공통 컴포넌트 제작

---
+++ DanceMachine/DanceMachine/Common/Components/ToastView.swift
@@ -0,0 +1,38 @@
+//
+//  ToastView.swift
+//  DanceMachine
+//
+//  Created by 김진혁 on 11/5/25.
+//
+
+import SwiftUI
+
+struct ToastView: View {
+  var text: String
+  
+  var body: some View {
+    HStack {
+      
+      Image(systemName: "exclamationmark.circle.fill")
+        .foregroundStyle(Color.accentRedNormal)
+      
+      Text(text)
+        .font(.headline2Medium)
+        .foregroundStyle(.labelStrong)
+        .multilineTextAlignment(.center)
+        .fixedSize(horizontal: false, vertical: true)
+        .padding(.vertical, 12)
+        .frame(maxWidth: .infinity, minHeight: 43, alignment: .leading)
+      
+    }
+    .padding(.leading, 16)
+    .background(
+      RoundedRectangle(cornerRadius: 10)
+        .fill(Color.fillAssitive)
+    )
+  }
+}
+
+#Preview {
+  ToastView(text: "배고프다")
+}
