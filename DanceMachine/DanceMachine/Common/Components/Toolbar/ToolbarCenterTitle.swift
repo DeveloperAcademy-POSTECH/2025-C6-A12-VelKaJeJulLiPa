50907d75abf9af0cf554a5257f7fb8c82841d7d8
jHoon99 <eldkf1597@naver.com>
2025-11-06 17:27:20 +0900

    💄[design]#51 - 툴바 디자인

--- DanceMachine/DanceMachine/Common/Components/Toolbar/ToolbarCenterTitle.swift
+++ DanceMachine/DanceMachine/Common/Components/Toolbar/ToolbarCenterTitle.swift
@@ -8,24 +8,25 @@
 import SwiftUI
 
 struct ToolbarCenterTitle: ToolbarContent {
-    
-    let text: String
-    
-    var body: some ToolbarContent {
-        ToolbarItem(placement: .principal) {
-            Text(text)
-                .font(Font.system(size: 18, weight: .semibold)) // FIXME: - 폰트 수정
-                .foregroundStyle(Color.black) // FIXME: - 컬러 수정
-                .allowsHitTesting(false)
-        }
+  
+  let text: String
+  
+  var body: some ToolbarContent {
+    ToolbarItem(placement: .principal) {
+      Text(text)
+        .font(.heading1Medium)
+        .foregroundStyle(.labelStrong)
+        .allowsHitTesting(false)
+        .frame(maxWidth: .infinity)
     }
+  }
 }
 
 #Preview {
-    NavigationStack {
-        Text("Preview")
-            .toolbar {
-                ToolbarCenterTitle(text: "안녕하세요")
-            }
-    }
+  NavigationStack {
+    Text("Preview")
+      .toolbar {
+        ToolbarCenterTitle(text: "안녕하세요")
+      }
+  }
 }
