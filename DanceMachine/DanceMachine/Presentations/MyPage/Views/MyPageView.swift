965b53d56c2b7ce8418c05a864ecc4c5ff09070f
Paidion <dev.paidion@gmail.com>
2025-11-06 21:00:23 +0900

    ✨[Feat]#46 - 마이페이지 앱 버전 및 DirAct를 만든 사람들 화면 추가

--- DanceMachine/DanceMachine/Presentations/MyPage/Views/MyPageView.swift
+++ DanceMachine/DanceMachine/Presentations/MyPage/Views/MyPageView.swift
@@ -9,58 +9,61 @@ import SwiftUI
 import UserNotifications
 
 struct MyPageView: View {
-    
-    @EnvironmentObject private var router: NavigationRouter
-    
-    @State private var viewModel = MyPageViewModel()
-    
-    
-    var body: some View {
-        ZStack {
-            Color.white.ignoresSafeArea() // FIXME: - 컬러 수정
-            
-            VStack { // FIXME: - spacing 수정
-                //FIXME: - 글자 스타일 수정
-                HStack {
-                    Text("마이페이지")
-                        .font(.title)
-                        .foregroundStyle(Color.black) // FIXME: - 컬러 수정
-                        .bold()
-                    Spacer()
-                }
-                .padding()
-                
-                MyPageInfoRow(title: "ID", value: viewModel.myId)
-                MyPageNavigationRow(title: "나의 이름", value: viewModel.myName) {
-                    router.push(to: .mypage(.editName))
-                }
-                
-                ThickDivider()
-                
-                MyPageNavigationRow(title: "개인정보처리방침") {
-                    router.push(to: .mypage(.privacyPolicy))
-                }
-                MyPageNavigationRow(title: "서비스 이용약관") {
-                    router.push(to: .mypage(.termsOfUse))
-                }
-                MyPageNavigationRow(title: "알림 수신") {
-                    viewModel.openAppSettings()
-                }
-                
-                ThickDivider()
-                
-                MyPageNavigationRow(title: "계정 설정") {
-                    router.push(to: .mypage(.accountSetting))
-                }
-                
-                Spacer()
-            }
+  
+  @EnvironmentObject private var router: NavigationRouter
+  
+  @State private var viewModel = MyPageViewModel()
+  
+  
+  var body: some View {
+    ZStack {
+      Color.backgroundNormal.ignoresSafeArea()
+      
+      VStack {
+        HStack {
+          Text("마이페이지")
+            .font(.title2SemiBold)
+            .foregroundStyle(.labelStrong)
+          Spacer()
         }
+        .padding()
+        
+        MyPageInfoRow(title: "ID", value: viewModel.myId)
+        MyPageNavigationRow(title: "나의 이름", value: viewModel.myName) {
+          router.push(to: .mypage(.editName))
+        }
+        
+        ThickDivider()
+        
+        MyPageNavigationRow(title: "개인정보처리방침") {
+          router.push(to: .mypage(.privacyPolicy))
+        }
+        MyPageNavigationRow(title: "서비스 이용약관") {
+          router.push(to: .mypage(.termsOfUse))
+        }
+        MyPageNavigationRow(title: "알림 수신") {
+          viewModel.openAppSettings()
+        }
+        
+        ThickDivider()
+        
+        MyPageNavigationRow(title: "계정 설정") {
+          router.push(to: .mypage(.accountSetting))
+        }
+        
+        MyPageInfoRow(title: "앱 버전", value: viewModel.appVersion)
+        
+        MyPageNavigationRow(title: "DirAct를 만든 사람들") {
+          router.push(to: .mypage(.appMaker))
+        }
+        Spacer()
+      }
     }
+  }
 }
 
 
 #Preview {
-    MyPageView()
-        .environmentObject(MyPageViewModel())
+  MyPageView()
+    .environmentObject(MyPageViewModel())
 }
