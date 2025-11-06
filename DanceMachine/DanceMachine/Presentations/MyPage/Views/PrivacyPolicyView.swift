87c29b0ba8cb9aa137ae5af43e1d5f52229d2ec9
Paidion <dev.paidion@gmail.com>
2025-11-06 20:58:40 +0900

    💄[Design]#46 - 개인정보처리방침 화면 Hi-fi 적용

--- DanceMachine/DanceMachine/Presentations/MyPage/Views/PrivacyPolicyView.swift
+++ DanceMachine/DanceMachine/Presentations/MyPage/Views/PrivacyPolicyView.swift
@@ -9,31 +9,30 @@ import SwiftUI
 import WebKit
 
 struct PrivacyPolicyView: View {
-    
-    @State private var isLoading: Bool = true
-    
-    private let privacyPolicyURL = URL(string: "https://mammoth-eyelash-f4f.notion.site/29610840462c8014ba1be32d01ef3edb")!
-    
-    
-    var body: some View {
-        ZStack {
-            Color.white.ignoresSafeArea()  // FIXME: - 컬러 수정
-            
-            WebView(url: privacyPolicyURL, isLoading: $isLoading)
-                .opacity(isLoading ? 0 : 1)
-                .toolbar {
-                    ToolbarLeadingBackButton(icon: .chevron)
-                    ToolbarCenterTitle(text: "개인정보처리방침")
-                }
-            
-            if isLoading {
-                ProgressView()
-            }
+  
+  @State private var isLoading: Bool = true
+  
+  private let privacyPolicyURL = URL(string: "https://mammoth-eyelash-f4f.notion.site/29610840462c8014ba1be32d01ef3edb")!
+  
+  
+  var body: some View {
+    ZStack {
+      Color.backgroundNormal.ignoresSafeArea()
+      
+      WebView(url: privacyPolicyURL, isLoading: $isLoading)
+        .opacity(isLoading ? 0 : 1)
+        .toolbar {
+          ToolbarLeadingBackButton(icon: .chevron)
+          ToolbarCenterTitle(text: "개인정보처리방침")
         }
-        
+      
+      if isLoading {
+        ProgressView()
+      }
     }
+  }
 }
 
 #Preview {
-    PrivacyPolicyView()
+  PrivacyPolicyView()
 }
