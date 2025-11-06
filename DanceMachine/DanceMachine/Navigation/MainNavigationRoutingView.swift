adc192263f3c07fbe5f799bc4ff64ee43ea32de9
Paidion <dev.paidion@gmail.com>
2025-11-06 20:59:30 +0900

    ✨[Feat]#46 - DirAct를 만든 사람들 화면 추가

--- DanceMachine/DanceMachine/Navigation/MainNavigationRoutingView.swift
+++ DanceMachine/DanceMachine/Navigation/MainNavigationRoutingView.swift
@@ -33,6 +33,8 @@ struct MainNavigationRoutingView: View {
                     TermsOfUseView().toolbar(.hidden, for: .tabBar)
                 case .accountSetting:
                     AccountSettingView().toolbar(.hidden, for: .tabBar)
+				case .appMaker:
+					AppMakerView().toolbar(.hidden, for: .tabBar)
                 }
             case .teamspace(let route):
                 switch route {
@@ -72,8 +74,61 @@ struct MainNavigationRoutingView: View {
                 }
             }
         }
-        .hideBackButton()
-        .dismissKeyboardOnTap()
-        .environmentObject(router)
+      case .mypage(let route):
+        switch route {
+        case .profile:
+          MyPageView()
+        case .editName:
+          EditNameView().toolbar(.hidden, for: .tabBar)
+        case .privacyPolicy:
+          PrivacyPolicyView().toolbar(.hidden, for: .tabBar)
+        case .termsOfUse:
+          TermsOfUseView().toolbar(.hidden, for: .tabBar)
+        case .accountSetting:
+          AccountSettingView().toolbar(.hidden, for: .tabBar)
+        case .appMaker:
+          AppMakerView().toolbar(.hidden, for: .tabBar)
+        }
+      case .teamspace(let route):
+        switch route {
+        case .list:
+          TeamspaceListView().toolbar(.hidden, for: .tabBar)
+        case .create:
+          CreateTeamspaceView().toolbar(.hidden, for: .tabBar)
+        case .setting:
+          TeamspaceSettingView().toolbar(.hidden, for: .tabBar)
+        }
+      case .project(let route):
+        switch route {
+        case .create:
+          CreateProjectView().toolbar(.hidden, for: .tabBar)
+        }
+      case .video(let route):
+        switch route {
+        case .list(let tracksId, let sectionId, let trackName):
+          VideoListView(
+            tracksId: tracksId,
+            sectionId: sectionId,
+            trackName: trackName
+          )
+        case .section(let section, let tracksId, let trackName, let sectionId):
+          SectionEditView(
+            sections: section,
+            tracksId: tracksId,
+            trackName: trackName,
+            sectionId: sectionId
+          )
+        case .play(let videoId, let videoTitle, let videoURL):
+          VideoView(
+            videoId: videoId,
+            videoTitle: videoTitle,
+            videoURL: videoURL
+          )
+        }
+      }
     }
+    .hideBackButton()
+    .dismissKeyboardOnTap()
+    .environmentObject(router)
+  }
 }
