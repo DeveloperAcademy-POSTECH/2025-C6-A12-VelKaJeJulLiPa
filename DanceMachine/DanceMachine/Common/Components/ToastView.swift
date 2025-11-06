d0905cba3e35aa4711b710dd72cb8cbd3be97693
jHoon99 <eldkf1597@naver.com>
2025-11-06 17:27:01 +0900

    ✨[Feat]#51 - 벨코 토스트뷰 복붙

---
+++ DanceMachine/DanceMachine/Common/Components/ToastView.swift
@@ -0,0 +1,58 @@
+//
+//  ToastView.swift
+//  DanceMachine
+//
+//  Created by 김진혁 on 11/5/25.
+//
+
+import SwiftUI
+
+struct ToastView: View {
+  var text: String
+  var icon: ToastIcon
+  
+  var body: some View {
+    HStack {
+      
+      Image(systemName: icon.icon)
+        .foregroundStyle(icon.iconColor)
+      
+      Text(text)
+        .font(.headline2Medium)
+        .foregroundStyle(.labelStrong)
+        .multilineTextAlignment(.center)
+        .fixedSize(horizontal: false, vertical: true)
+        .padding(.vertical, 12)
+        .frame(maxWidth: .infinity, minHeight: 43, alignment: .leading)
+      
+    }
+    .padding(.leading, 16)
+    .background(
+      RoundedRectangle(cornerRadius: 10)
+        .fill(Color.fillAssitive)
+    )
+  }
+}
+
+enum ToastIcon: String {
+  case warning
+  case check
+  
+  var icon: String {
+    switch self {
+    case .warning: return "exclamationmark.circle.fill"
+    case .check: return "checkmark.circle.fill"
+    }
+  }
+  
+  var iconColor: Color {
+    switch self {
+    case .warning: return .accentRedNormal
+    case .check: return .secondaryNormal
+    }
+  }
+}
+
+#Preview {
+  ToastView(text: "배고프다", icon: .check)
+}
