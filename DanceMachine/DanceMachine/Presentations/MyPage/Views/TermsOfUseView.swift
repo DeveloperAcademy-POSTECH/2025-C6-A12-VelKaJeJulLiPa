e56132330a86b99cd9f11ed3201bbad79b5a3e78
Paidion <dev.paidion@gmail.com>
2025-11-06 20:58:29 +0900

    💄[Design]#46 - 이용 약관 화면 Hi-fi 적용

--- DanceMachine/DanceMachine/Presentations/MyPage/Views/TermsOfUseView.swift
+++ DanceMachine/DanceMachine/Presentations/MyPage/Views/TermsOfUseView.swift
@@ -8,31 +8,29 @@
 import SwiftUI
 
 struct TermsOfUseView: View {
-    @State private var isLoading = false
-    
-    private let termsOfUseURL = URL(string: "https://mammoth-eyelash-f4f.notion.site/29610840462c8038a85bf08362518b03?source=copy_link")!
-    
-    var body: some View {
-        ZStack {
-            Color.white.ignoresSafeArea()  // FIXME: - 컬러 수정
-            
-            WebView(url: termsOfUseURL, isLoading: $isLoading)
-                .opacity(isLoading ? 0 : 1)
-                .toolbar {
-                    ToolbarLeadingBackButton(icon: .chevron)
-                    ToolbarCenterTitle(text: "서비스 이용약관")
-                }
-            
-            if isLoading {
-                ProgressView()
-            }
+  @State private var isLoading = false
+  
+  private let termsOfUseURL = URL(string: "https://mammoth-eyelash-f4f.notion.site/29610840462c8038a85bf08362518b03?source=copy_link")!
+  
+  var body: some View {
+    ZStack {
+      Color.backgroundNormal.ignoresSafeArea()
+      
+      WebView(url: termsOfUseURL, isLoading: $isLoading)
+        .opacity(isLoading ? 0 : 1)
+        .toolbar {
+          ToolbarLeadingBackButton(icon: .chevron)
+          ToolbarCenterTitle(text: "서비스 이용약관")
         }
-        
-        
+      
+      if isLoading {
+        ProgressView()
+      }
     }
+  }
 }
 
 
 #Preview {
-    TermsOfUseView()
+  TermsOfUseView()
 }
