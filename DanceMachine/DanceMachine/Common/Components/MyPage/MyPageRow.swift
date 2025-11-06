393be1ca526def7dde3d4bddc9258a039cd8a66d
Paidion <dev.paidion@gmail.com>
2025-11-06 17:37:41 +0900

    💄[Design]#46 - 마이페이지 컴포넌트 Hi-fi 반영

--- DanceMachine/DanceMachine/Common/Components/MyPage/MyPageRow.swift
+++ DanceMachine/DanceMachine/Common/Components/MyPage/MyPageRow.swift
@@ -8,26 +8,29 @@
 import SwiftUI
 
 /// 마이페이지에서 정보만 표시하는 행
-struct MyPageInfoRow: View { // FIXME: - Hi-fi 스타일 적용
-    let title: String
-    let value: String
-    
-    var body: some View {
-        HStack {
-            Text(title)
-                .font(.system(size: 16))
-                .foregroundStyle(Color.black) // FIXME: - 컬러 수정
-            
-            Spacer()
-            
-            Text(value)
-                .font(.system(size: 16))
-                .foregroundStyle(Color.black) // FIXME: - 컬러 수정
-        }
-        .padding()
+struct MyPageInfoRow: View {
+  let title: String
+  let value: String
+  
+  var body: some View {
+    HStack {
+      Text(title)
+        .font(.headline2Medium)
+        .foregroundStyle(.labelStrong)
+      
+      Spacer()
+      
+      Text(value)
+        .font(.headline2Medium)
+        .foregroundStyle(.labelNormal)
     }
+    .padding()
+  }
 }
 
 #Preview {
+  ZStack{
+    Color.backgroundNormal.ignoresSafeArea()
     MyPageInfoRow(title: "TItle", value: "value")
+  }
 }
