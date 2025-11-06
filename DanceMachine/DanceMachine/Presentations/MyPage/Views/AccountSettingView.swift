7570a9e9e6d95b898d0e4c8b003e506fdd201b94
Paidion <dev.paidion@gmail.com>
2025-11-06 20:58:11 +0900

    💄[Design]#46 - 계정 설정 화면 Hi-fi 적용

--- DanceMachine/DanceMachine/Presentations/MyPage/Views/AccountSettingView.swift
+++ DanceMachine/DanceMachine/Presentations/MyPage/Views/AccountSettingView.swift
@@ -13,71 +13,65 @@ struct AccountSettingView: View {
   
   @State private var viewModel = AccountSettingViewModel()
   
-  @State private var isPresentingSignOutSheet: Bool = false     // 로그아웃 시트 제어
-  @State private var isPresentingDeleteUserSheet: Bool = false  // 회원탈퇴 시트 제어
+  @State private var isSignOutAlertPresented: Bool = false
+  @State private var isDeleteUserAlertPresented: Bool = false
   
   
   var body: some View {
     ZStack {
-      Color.white.ignoresSafeArea() // FIXME: - 컬러 수정
+      Color.backgroundNormal.ignoresSafeArea()
       
       VStack {
-        MyPageInfoRow(
-          title: "ID",
-          value: viewModel.myId
-        )
+        MyPageInfoRow(title: "ID", value: viewModel.myId)
         
         Spacer()
         
         ActionButton(
           title: "로그아웃",
-          color: Color.orange, // FIXME: - 컬러 수정
+          color: .secondaryStrong,
           height: 47,
-          action: { isPresentingSignOutSheet = true }
+          action: { isSignOutAlertPresented = true }
         )
         .padding()
         
         Button {
-          isPresentingDeleteUserSheet = true
+          isDeleteUserAlertPresented = true
         } label: {
           Text("회원탈퇴")
-            .foregroundStyle(.red) // FIXME: - 컬러 수정
+            .foregroundStyle(.accentRedStrong)
         }
       }
-    }
-    .sheet(isPresented: $isPresentingSignOutSheet) {
-      BottomConfirmSheetView(
-        titleText: "로그아웃하시겠어요?",
-        primaryText: "로그아웃") {
-          Task {
-             try await viewModel.signOut()
-            router.popToRootView()
-          }
-        }
-    }
-    .sheet(isPresented: $isPresentingDeleteUserSheet) {
-      BottomConfirmSheetView(
-        titleText: "탈퇴하시겠어요?\n모든 정보가 삭제됩니다.",
-        primaryText: "탈퇴") {
-          Task {
-            try await viewModel.deleteUserAccount()
-            router.popToRootView()
-          }
-        }
     }
     .toolbar {
       ToolbarLeadingBackButton(icon: .chevron)
       ToolbarCenterTitle(text: "계정 설정")
     }
-    
+    .alert(
+      "로그아웃",
+      isPresented: $isSignOutAlertPresented
+    ) {
+      Button("취소", role: .cancel) {}
+      Button("로그아웃", role: .destructive) {
+        Task { try await viewModel.signOut() }
+      }
+    } message: {
+      Text("정말 로그아웃하시겠어요?")
+    }
+    .alert(
+      "회원탈퇴",
+      isPresented: $isDeleteUserAlertPresented
+    ) {
+      Button("취소", role: .cancel) {}
+      Button("탈퇴", role: .destructive) {
+        Task { try await viewModel.deleteUserAccount() }
+      }
+    } message: {
+      Text("회원 정보가 삭제되고 되돌릴 수 없습니다.")
+    }
   }
 }
-
 
 
 #Preview {
   AccountSettingView()
 }
-
-
-
