e81adf7052c216cb467d51da5ed42dced9028a56
Paidion <dev.paidion@gmail.com>
2025-11-10 01:05:27 +0900

    ✨[Feat]#47 - 신고 유형, 신고 콘텐츠 유형, 토스트 확인해야 하는 화면 Enum 생성
    - 신고 이후 토스트 메시지를 다양한 화면에서 확인할 수 있기 때문에 ReportReceiveViewType 를 생성

---
+++ DanceMachine/DanceMachine/Common/Enum/Report/ReportToastReceiveViewType.swift
@@ -0,0 +1,16 @@
+//
+//  ReportToastReceiveViewType.swift
+//  DanceMachine
+//
+//  Created by Paidion on 11/10/25.
+//
+
+import Foundation
+
+/// 신고 관련 토스트 메시지를 받는 화면
+enum ReportToastReceiveViewType {
+  case videoListView
+  case videoView
+  case replySheet
+}
+ 
