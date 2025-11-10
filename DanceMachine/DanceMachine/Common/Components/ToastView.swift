a2703ae7e71bc9aaf4525997c2fecba6277e4c51
Velko <wlsgurrla716@naver.com>
2025-11-10 15:50:13 +0900

    🐛[Fix]#47 - 토스트 뷰 머지 충돌 코드 수정

--- DanceMachine/DanceMachine/Common/Components/ToastView.swift
+++ DanceMachine/DanceMachine/Common/Components/ToastView.swift
@@ -35,25 +35,6 @@ struct ToastView: View {
   }
 }
 
-enum ToastIcon: String {
-  case warning
-  case check
-  
-  var icon: String {
-    switch self {
-    case .warning: return "exclamationmark.circle.fill"
-    case .check: return "checkmark.circle.fill"
-    }
-  }
-  
-  var iconColor: Color {
-    switch self {
-    case .warning: return .accentRedNormal
-    case .check: return .secondaryNormal
-    }
-  }
-}
-
 #Preview {
   ToastView(text: "배고프다", icon: .check)
 }
