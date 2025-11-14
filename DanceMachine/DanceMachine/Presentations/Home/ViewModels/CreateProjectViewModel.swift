ffe1edff2619493d847354f75aae91464a1d9623
Velko <wlsgurrla716@naver.com>
2025-11-14 17:58:41 +0900

    🔧[Chore]#44 - 프로젝트 생성 시, 팀 스페이스 updated_at 시간 갱신 추가

--- DanceMachine/DanceMachine/Presentations/Home/ViewModels/CreateProjectViewModel.swift
+++ DanceMachine/DanceMachine/Presentations/Home/ViewModels/CreateProjectViewModel.swift
@@ -9,23 +9,34 @@ import Foundation
 
 @Observable
 final class CreateProjectViewModel {
-    
-    var currentTeamspace: Teamspace? { FirebaseAuthManager.shared.currentTeamspace }
-    
-    /// 해당 팀스페이스의 프로젝트를 생성하는 메서드입니다.
-    /// - Parameters:
-    ///     - projectName: 프로젝트 이름 설정
-    func createProject(projectName: String) async throws {
-        do {
-            let project: Project = .init(
-                projectId: UUID(),
-                teamspaceId: self.currentTeamspace?.teamspaceId.uuidString ?? "",
-                creatorId: FirebaseAuthManager.shared.userInfo?.userId ?? "",
-                projectName: projectName
-            )
-            try await FirestoreManager.shared.create(project)
-        } catch {
-            print("error: \(error.localizedDescription)") // FIXME: - 적절한 에러 분기 처리
-        }
+  
+  var currentTeamspace: Teamspace? { FirebaseAuthManager.shared.currentTeamspace }
+  
+  /// 해당 팀스페이스의 프로젝트를 생성하는 메서드입니다.
+  /// - Parameters:
+  ///     - projectName: 프로젝트 이름 설정
+  func createProject(projectName: String) async throws {
+    do {
+      // FIXME: - batch 추가하기
+      /// 프로젝트 생성
+      let project: Project = .init(
+        projectId: UUID(),
+        teamspaceId: self.currentTeamspace?.teamspaceId.uuidString ?? "",
+        creatorId: FirebaseAuthManager.shared.userInfo?.userId ?? "",
+        projectName: projectName
+      )
+      try await FirestoreManager.shared.create(project)
+      
+      /// 팀 스페이스 updated_at 갱신
+      // TODO: 프로젝트 생성은 updated_at 갱신 완료했고, 프로젝트 수정,삭제 될 때도 갱신되게 하기
+      try await FirestoreManager.shared.updateTimestampField(
+        field: .update,
+        in: .teamspace,
+        documentId: self.currentTeamspace?.teamspaceId.uuidString ?? ""
+      )
+      
+    } catch {
+      print("error: \(error.localizedDescription)") // FIXME: - 적절한 에러 분기 처리
     }
+  }
 }
