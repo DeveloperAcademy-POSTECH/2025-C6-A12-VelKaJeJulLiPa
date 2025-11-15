7811aa28dbde4ef5f49173889a9f6ed7c09af336
Paidion <dev.paidion@gmail.com>
2025-11-15 20:19:58 +0900

    🐛[Fix]#49 - 팀스페이스 Published 프로퍼티 래퍼 추가
    - DanceMachineApp 의 팀스페이스 세팅 상태를 감지해서 팀스페이스 세팅 이후에 딥링크 처리를 해주기 위함

--- DanceMachine/DanceMachine/Service/FirebaseAuthManager.swift
+++ DanceMachine/DanceMachine/Service/FirebaseAuthManager.swift
@@ -31,7 +31,7 @@ final class FirebaseAuthManager: ObservableObject {
   private var currentNonce: String?
   
   /// 현재 선택된 유저의 팀스페이스 입니다.
-  var currentTeamspace: Teamspace?
+  @Published var currentTeamspace: Teamspace?
   var isSigningIn: Bool = false
   
   private init() {
