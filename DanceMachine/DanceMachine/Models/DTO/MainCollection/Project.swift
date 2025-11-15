25277a65db02f9fbcfa1eb0742c2cfcbce77fc94
Velko <wlsgurrla716@naver.com>
2025-11-16 00:02:48 +0900

    🔧[Chore]#44 - ProjectDTO update_at 추가

--- DanceMachine/DanceMachine/Models/DTO/MainCollection/Project.swift
+++ DanceMachine/DanceMachine/Models/DTO/MainCollection/Project.swift
@@ -8,36 +8,40 @@
 import Foundation
 
 struct Project: Codable, Equatable {
-    let projectId: UUID
-    let teamspaceId: String
-    let creatorId: String
-    let projectName: String
-
-    init(
-        projectId: UUID,
-        teamspaceId: String,
-        creatorId: String,
-        projectName: String
-    ) {
-        self.projectId = projectId
-        self.teamspaceId = teamspaceId
-        self.creatorId = creatorId
-        self.projectName = projectName
-    }
-
-    enum CodingKeys: String, CodingKey {
-        case projectId   = "project_id"
-        case teamspaceId = "teamspace_id"
-        case creatorId   = "creator_id"
-        case projectName = "project_name"
-    }
+  let projectId: UUID
+  let teamspaceId: String
+  let creatorId: String
+  let projectName: String
+  var updatedAt: Date?
+  
+  init(
+    projectId: UUID,
+    teamspaceId: String,
+    creatorId: String,
+    projectName: String,
+    updatedAt: Date? = nil
+  ) {
+    self.projectId = projectId
+    self.teamspaceId = teamspaceId
+    self.creatorId = creatorId
+    self.projectName = projectName
+    self.updatedAt = updatedAt
+  }
+  
+  enum CodingKeys: String, CodingKey {
+    case projectId   = "project_id"
+    case teamspaceId = "teamspace_id"
+    case creatorId   = "creator_id"
+    case projectName = "project_name"
+    case updatedAt   = "updated_at"
+  }
 }
 
 extension Project: Identifiable {
-    var id: UUID { projectId }
+  var id: UUID { projectId }
 }
 
 extension Project: EntityRepresentable {
-    var entityName: CollectionType { .project }
-    var documentID: String { projectId.uuidString }
+  var entityName: CollectionType { .project }
+  var documentID: String { projectId.uuidString }
 }
