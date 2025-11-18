589537826498429aae2a9aba677ab6709bb08658
jHoon99 <eldkf1597@naver.com>
2025-11-18 18:00:55 +0900

    ✨[Feat]#98 - 에러메세지 지정

--- DanceMachine/DanceMachine/Common/Enum/Error/SectionEditError.swift
+++ DanceMachine/DanceMachine/Common/Enum/Error/SectionEditError.swift
@@ -15,22 +15,22 @@ enum SectionEditError: Error {
   var debugMsg: String {
     switch self {
     case .createError:
-      return ""
+      return "섹션 생성 에러"
     case .updateError:
-      return ""
+      return "섹션 수정 에러"
     case .deleteError:
-      return ""
+      return "섹션 삭제 에러"
     }
   }
   
   var userMsg: String {
     switch self {
     case .createError:
-      return ""
+      return "파트를 생성하는 중에 문제가 발생했습니다.\n네트워크 확인 후 다시 시도해 주세요."
     case .updateError:
-      return ""
+      return "파트를 수정하는 중에 문제가 발생했습니다.\n네트워크 확인 후 다시 시도해 주세요."
     case .deleteError:
-      return ""
+      return "파트를 삭제하는 중에 문제가 발생했습니다.\n네트워크 확인 후 다시 시도해 주세요."
     }
   }
 }
