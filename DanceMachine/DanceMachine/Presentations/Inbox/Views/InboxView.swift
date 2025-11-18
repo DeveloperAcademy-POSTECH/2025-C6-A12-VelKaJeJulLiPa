bb14bf9efb827f27ecab0497c4748db5fab87b72
Paidion <dev.paidion@gmail.com>
2025-11-18 19:31:09 +0900

    🔧[Chore]#92 - 알림 화면 로딩상태 로직 수정 및 에러화면 추가
    - 초기 로딩 상태 추가 및 분기처리
    - 에러 상태 화면 추가

--- DanceMachine/DanceMachine/Presentations/Inbox/Views/InboxView.swift
+++ DanceMachine/DanceMachine/Presentations/Inbox/Views/InboxView.swift
@@ -15,10 +15,23 @@ struct InboxView: View {
     ZStack {
       Color.backgroundNormal.ignoresSafeArea()
       
-      VStack(spacing: 0) {
-        if viewModel.isLoading && viewModel.inboxNotifications.isEmpty {
-          LoadingSpinner()
-            .frame(maxWidth: 28, maxHeight: 28, alignment: .center)
+      VStack {
+        if viewModel.showErrorMessage {
+          VStack(spacing: 16, ) {
+            Text("알림 불러오기를 실패했습니다.\n네트워크를 확인해주세요")
+              .multilineTextAlignment(.center)
+            Image(systemName: "arrow.trianglehead.clockwise")
+              .symbolEffect(.rotate.wholeSymbol, options: .nonRepeating, value: viewModel.isRefreshing)
+              .onTapGesture {
+                Task { await viewModel.refresh() }
+              }
+          }
+        } else if viewModel.isLoading && viewModel.inboxNotifications.isEmpty {
+          ScrollView{
+            ForEach(0..<4, id: \.self) { _ in
+              SkeletonInboxNotificationRow()
+            }
+          }
         } else if viewModel.inboxNotifications.isEmpty {
           GeometryReader { geometry in
             ScrollView {
