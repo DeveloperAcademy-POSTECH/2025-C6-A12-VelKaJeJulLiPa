8fcc05c7ca3b64f9231b19ed6e4ec1b270fe016d
Paidion <dev.paidion@gmail.com>
2025-11-21 17:06:22 +0900

    ✨[Feat]#91 - 온보딩 화면 라우팅에 따른 화면 매칭

--- DanceMachine/DanceMachine/Navigation/AuthNavigationRoutingView.swift
+++ DanceMachine/DanceMachine/Navigation/AuthNavigationRoutingView.swift
@@ -11,15 +11,23 @@ struct AuthNavigationRoutingView: View {
   @EnvironmentObject var router: AuthRouter
   @State var destination: AuthRoute
   
-    var body: some View {
-      Group {
-        switch destination {
-        case .login:
-          LoginView()
-        }
+  var body: some View {
+    Group {
+      switch destination {
+      case .login:
+        LoginView()
+      case .termsAgree:
+        TermsAgreeView()
+      case .initialNameSetting:
+        NameSettingView()
+      case .privacyPolicy:
+        PrivacyPolicyView()
+      case .termsOfUse:
+        TermsOfUseView()
       }
-      .hideBackButton()
-      .dismissKeyboardOnTap()
-      .environmentObject(router)
     }
+    .hideBackButton()
+    .dismissKeyboardOnTap()
+    .environmentObject(router)
+  }
 }
